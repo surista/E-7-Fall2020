a992a01ef2639651d5fc3cc360479baf3f375fcb
s.urista <surista@g.harvard.edu>
2020-10-19 22:58:29 +0900

    update find_url.py

--- Day08/find_url.py
+++ Day08/find_url.py
@@ -17,21 +17,22 @@ def find_url(website):
     with urllib.request.urlopen(website) as f:
         text = f.read().decode('utf-8')
 
-    # set up tags for searching
-    # find first link anchor.
+    # set up tags for searching, find first link anchor.
     # 9 refers to length of the <a href=" tag
 
     href_tag = '<a href="'
     first_href = text.find(href_tag)
     end_href = text.find('"', first_href + 9)
-    first_a_end = text.find('</a')
+
+    link_end = text.find('</a')
+    link_start = text.rfind(">", 1,link_end)
 
     # generate string segments
     first_url = text[first_href + 9:end_href]
-    first_text = text[end_href + 2:first_a_end]
+    first_text = text[link_start+1:link_end].strip()
 
     return (first_url, first_text)
 
 
-url = 'https://www.python.org/'
+url = 'https://www.harvard.edu/'
 print(find_url(url))
