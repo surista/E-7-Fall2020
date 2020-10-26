1597d79072cc8e16281474aea4443608a379dbaa
s.urista <surista@g.harvard.edu>
2020-10-26 16:05:41 +0900

    updated HW9

--- Day09/next_month.py
+++ Day09/next_month.py
@@ -6,25 +6,28 @@ Oct 2020
 Module documentation goes here
 """
 
-# month_names = ['January', 'February', 'March', 'April', 'May', 'June',
-#                 'July', 'August', 'September', 'October', 'November', 'December']
-#
-# def next_month(name: str) -> str:
-#     "Return a stream of the following months"
-#     if name not in month_names:
-#         raise ValueError
-#     yield month_names
+month_names = ['January', 'February', 'March', 'April', 'May', 'June',
+                'July', 'August', 'September', 'October', 'November', 'December']
 
-#
-# gen = next_month('Octofber')
-#
+def next_month(name: str) -> str:
+    "Return a stream of the following months"
+    if name not in month_names:
+        raise ValueError
+    month = month_names.index(name) + 1
+    while (True):
+        month = (month % 12)
+        yield month_names[month]
+        month += 1
 
-def squares(nums):
-    result = []
-    for i in nums:
-        result.append(i*i)
-    return result
 
-my_nums = squares([1,2,3,4,5])
+def test_months():
+    gen = next_month('October')
+    lst = [next(gen) for i in range(15)]
+    assert(lst == ['November', 'December', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December', 'January'])
 
-print(my_nums)
+    gen = next_month('December')
+    assert next(gen) == 'January'
+
+    print('Success!')
+
+test_months()
