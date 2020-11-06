c32f6db04b174a44e35ba6f651949bc60d3d7eb6
s.urista <surista@g.harvard.edu>
2020-11-06 18:33:41 +0900

    update wk10

--- Day10/beautiful_Jam.py
+++ Day10/beautiful_Jam.py
@@ -11,15 +11,24 @@ from bs4 import BeautifulSoup
 
 url = "https://spacejam.com/"
 html_content = requests.get(url).text
-
 webpage = requests.get("https://spacejam.com")
 soup = BeautifulSoup(html_content, 'html.parser')
 pretty_soup = soup.prettify()
-print(pretty_soup)
-all_links = [link for link in soup.find_all("a")]
-test = []
-# for item in range(6):
-#     print(all_links[item])
 
-# print(test)
+links = soup.find_all("a")
 
+print(links)
+# all_links = [(link['href'], link.string) for link in soup.find_all("a")]
+# print(all_links, len(all_links))
+
+# all_links = [(link, link.string) for link in soup.find_all("a")]
+# print(all_links, len(all_links))
+#
+# for link i all_links:
+#     if
+#
+#
+# all_links = [link for link in soup.find_all("a")]
+# print(len(all_links))
+# for item in all_links:
+#     print(item)
