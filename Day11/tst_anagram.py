c0830ba24531fe3f654f9d7cff53457cae86eb9a
suris <surista@gmail.com>
2020-11-11 15:08:55 +0900

    update wk11

--- Day11/tst_anagram.py
+++ Day11/tst_anagram.py
@@ -7,35 +7,37 @@ find anagrams in list of words
 """
 
 from typing import List
-from collections import OrderedDict
+from collections import defaultdict
 
 def find_anagrams(path: str) -> List:
     """
     "Find the largest set of anagrams, return as sorted list"
     """
+
     try:
         with open(path, 'r') as f:
             words_list = [word.strip() for word in f]
 
-        reversed_word = [(''.join(sorted(word))) for word in words_list]
-
+        # create list of all words sorted alphabetically
+        sorted_word = [(''.join(sorted(word))) for word in words_list]
 
     except FileNotFoundError:
-        print(f"Houson, we have a problem, could not find file {filepath}")
+        print(f"Houston, we have a problem, could not find file {path}")
 
-    print(reversed_word[0:10])
-    print(words_list[0:10])
+    # pair each word to its sorted self
+    pairs = [(sorted_word[x], words_list[x]) for x in range(0, len(words_list))]
 
-    sol = {}
-    for x in words_list:
-        if x not in sol:
-            sol[x] = 1
-        else:
-            sol[x] += 1
+    agrams =defaultdict(list)
+    for x, y in pairs:
+        agrams[x].append(y)
 
-    rev = [(x, y) for y, x in sol.items()]
+    #items of length 1 means not an anagram
+    sol = []
+    for item in agrams:
+        if len(agrams[item]) > 1:
+            sol.append((len(agrams[item]),agrams[item]))
 
-    return None
+    return sorted(sol, reverse=True)
 
 filename = "words.txt"
 print(find_anagrams(filename))
