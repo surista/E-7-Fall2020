c33b85b1420b51cece7dfda4f3d63d30043a8477
s.urista <surista@g.harvard.edu>
2020-11-26 14:18:17 +0900

    update wk12

--- Day12/parkers_double_table.py
+++ Day12/parkers_double_table.py
@@ -19,26 +19,25 @@ def longest_repeat(text):
 
     sol = (0,0,0)
     count = 2
+
     while True:
 
         d = defaultdict(list)
         for i in range(len(text)):
-            d[text[i:i+count]].append(i)
-        del_list = [(item, d[item]) for item in d if len(item) > 1 and len(d[item]) > 1]
-        if len(del_list) == 0:
-            return sol
-        else:
+            d[text[i:i + count]].append(i)
+            del_list = [(item, d[item]) for item in d if len(item) > 1 and len(d[item]) > 1]
+            print(del_list)
             temp = [(item[1][0], item[1][1], len(item[0])) for item in del_list]
             sol = (temp[0][0], temp[0][1], temp[0][2])
-            print(sol)
-        count += 1
+            count += 1
 
+    return sol
 
-# text = read_fasta_file("pKLMF-FX.fasta")
-# print(longest_repeat(text))
-#
+text = read_fasta_file("pACYC184_short.fasta")
+print(longest_repeat(text))
+
 # text = read_fasta_file("pACYC184.fasta")
 # print(longest_repeat(text))
 
-text = read_fasta_file("ecoli.fasta")
-print(longest_repeat(text))
+# text = read_fasta_file("ecoli.fasta")
+# print(longest_repeat(text))
